3c6281ea97b4356db878c4d747336db73d4b506b
galik41 <91020468+galik41@users.noreply.github.com>
2022-01-07 01:18:54 +0200

    first commit

---
+++ theWeatherHere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  theWeatherHere
+//
+//  Created by gal karady on 05/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
